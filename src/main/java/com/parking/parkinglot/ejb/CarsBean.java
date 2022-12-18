b3affbad349afdd16dbe55b498d960539e3a4c19
Dulea Mihai <mihaidulea@yahoo.com>
2022-12-18 17:53:59 +0200

    CRUD for cars

--- src/main/java/com/parking/parkinglot/ejb/CarsBean.java
+++ src/main/java/com/parking/parkinglot/ejb/CarsBean.java
@@ -2,6 +2,7 @@ package com.parking.parkinglot.ejb;
 
 import com.parking.parkinglot.common.CarDto;
 import com.parking.parkinglot.entities.Car;
+import com.parking.parkinglot.entities.User;
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
@@ -10,6 +11,7 @@ import jakarta.persistence.TypedQuery;
 import jdk.jpackage.internal.Log;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -33,7 +35,18 @@ public class CarsBean {
             throw new EJBException(e);
         }
     }
+    public void createCar(String licensePlate, String parkingSpot, Long userId){
+        LOG.info("createCar");
 
+        Car car=new Car();
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User user=entityManager.find(User.class, userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+        entityManager.persist(car);
+    }
     private List<CarDto> copyCarsToDto(List<Car> cars) {
         List<CarDto> carDtos = new ArrayList<>();
         for(Car car : cars){
@@ -41,5 +54,38 @@ public class CarsBean {
 
         }
         return carDtos;
+    }
+
+    public CarDto findById(Long carId) {
+        Car car = entityManager.find(Car.class,carId);
+        CarDto carDto = new CarDto(car.getId(),car.getLicensePlate(),car.getParkingSpot(),car.getOwner().getUsername());
+        return carDto;
+    }
+
+    public void updateCar(Long carId, String licensePlate, String parkingSpot, Long userId) {
+        LOG.info("updateCar");
+
+        Car car=entityManager.find(Car.class,carId);
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        // remove this car from the old owner
+        User oldUser=car.getOwner();
+        oldUser.getCars().remove(car);
+
+        //add the car to its new owner
+        User user=entityManager.find(User.class,userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+    }
+
+    public void deleteCarsByIds(Collection<Long> carIds) {
+        LOG.info("deleteCarsByIds");
+        for(Long carId:carIds)
+        {
+            Car car=entityManager.find(Car.class,carId);
+            entityManager.remove(car);
+        }
+
     }
 }
--- src/main/webapp/WEB-INF/pages/cars.jsp
+++ src/main/webapp/WEB-INF/pages/cars.jsp
@@ -3,26 +3,32 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <t:pageTemplate pageTitle="Cars" >
-    <h1>Welcome to the parking Lot</h1>
-    <div class="container text-center">
-        <a></a>
-        <a href="${pageContext.request.contextPath}/AddCar" class="btn btn-primary btn-lg"> Add Car</a>
-        <a></a>
-        <c:forEach var="car" items="${cars}">
-            <div class="row">
-                <div class="col">
-                    ${car.licensePlate}
+    <h1>Cars</h1>
+    <form method="POST" action="${pageContext.request.contextPath}/Cars">
+        <button class="btn btn-danger" type="submit"> Delete Cars </button>
+        <div class="container text-center">
+            <a href="${pageContext.request.contextPath}/AddCar" class="btn btn-primary btn-lg"> Add Car</a>
+            <c:forEach var="car" items="${cars}">
+                <div class="row">
+                    <div class="col">
+                        <input type="checkbox" name="car_ids" value="${car.id}" />
+                    </div>
+                    <div class="col">
+                        ${car.licensePlate}
+                    </div>
+                    <div class="col">
+                            ${car.parkingSpot}
+                    </div>
+                    <div class="col">
+                            ${car.ownerName}
+                    </div>
+                    <div class="col">
+                        <a class="btn btn-secondary" href="${pageContext.request.contextPath}/EditCar?id=${car.id}">Edit Car</a>
+                    </div>
                 </div>
-                <div class="col">
-                        ${car.parkingSpot}
-                </div>
-                <div class="col">
-                        ${car.ownerName}
-                </div>
-            </div>
-        </c:forEach>
-    </div>
-
+            </c:forEach>
+        </div>
+    </form>
     </div>
     <h5>Free parking spots : ${numberOfFreeParkingSpots}</h5>
 
